1390c5ee6408f26b79ed94047ab3b2a410bc1355
Memphis <kvn_araneda@hotmail.com>
2025-02-03 14:10:24 -0300

    agrego función para sortear lista amigos y luego lo muestre en pantalla

--- app.js
+++ app.js
@@ -31,3 +31,14 @@ function agregarAmigosLista(){
     }
 }
 
+//Escribe una función que seleccione de manera aleatoria uno de los nombres almacenados en el 
+// array lista amigos. Usa Math.random() y Math.floor() para obtener un índice aleatorio.
+function sortearAmigo(){
+    let indice = Math.floor(Math.random() * listaAmigos.length);
+    console.log(listaAmigos[indice]);
+
+    //Actualizar el contenido del elemento de resultado utilizando document.getElementById()  
+    // e innerHTML para mostrar el amigo sorteado.
+    let resultado = document.getElementById("amigosLista");
+    resultado.innerHTML = listaAmigos[indice];
+}
