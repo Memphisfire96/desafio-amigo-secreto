41339959a7bf9cded436d3f0da793c5ba7be7d53
Memphis <kvn_araneda@hotmail.com>
2025-02-03 13:25:23 -0300

    creación de funcion para agregar datos a la lista

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listaAmigos= [];
+
+//funcion mejorada
+function agregarAmigoMejorado(){
+    let entrada = document.getElementById("amigo");
+    let nombreAmigo = entrada.value.trim();
+    if (entrada.value == ""){
+        alert("por favor ingresa un nombre valido");
+    }else{
+        listaAmigos.push(nombreAmigo);
+        console.log(listaAmigos);
+        //limpiar la entrada en el html
+        entrada.value = "";
+    }
+
+}
